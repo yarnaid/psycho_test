df4e70120d9acf365add1dec2072a05ea42cc066
yarnaid <yarniad@gmail.com>
2016-05-14 21:38:40 +0300

    db

--- psycho_test/settings.py
+++ psycho_test/settings.py
@@ -74,13 +74,16 @@ WSGI_APPLICATION = 'psycho_test.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
+# heroku database
+import dj_database_url
+DATABASES =  {'default': dj_database_url.config()}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
